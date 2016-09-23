1267429a076658e006c519a4d56b33db030e7e75
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-09-23 15:05:41 +0200

    add `--quiet` to `tip_cat`

--- src/tip_cat.ml
+++ src/tip_cat.ml
@@ -5,25 +5,40 @@
 
 module A = Tip_ast
 
-let process_stdin () =
-  let l = Tip_util.parse_chan_exn stdin in
-  Format.printf "; from <stdin>@.";
-  Format.printf "@[<hv>%a@]@." (A.pp_list A.pp_stmt) l
+let quiet = ref false
 
-let process_file file =
-  let l = Tip_util.parse_file_exn file in
-  Format.printf "; file `%s`@." file;
-  Format.printf "@[<hv>%a@]@." (A.pp_list A.pp_stmt) l
+type input =
+  | Stdin
+  | File of string
+
+let string_of_input = function
+  | Stdin -> "<stdin>"
+  | File f -> Printf.sprintf "file `%s`" f
+
+let process i =
+  let l = match i with
+    | Stdin -> Tip_util.parse_chan_exn stdin
+    | File file -> Tip_util.parse_file_exn file
+  in
+  if not !quiet then (
+    Format.printf "; from %s@." (string_of_input i);
+    Format.printf "@[<hv>%a@]@." (A.pp_list A.pp_stmt) l
+  )
+
+let process_file f = process (File f)
 
 let options =
-  Arg.align [ ]
+  Arg.align [
+    "--quiet", Arg.Set quiet, " quiet mode (check only)";
+    "-q", Arg.Set quiet, " short for --quiet";
+  ]
 
 let () =
   let l = ref [] in
   Arg.parse options (fun s -> l := s :: !l) "usage: tip-cat [file]*";
   try
     if !l=[]
-    then process_stdin()
+    then process Stdin
     else List.iter process_file (List.rev !l)
   with e ->
     Printexc.print_backtrace stdout;
