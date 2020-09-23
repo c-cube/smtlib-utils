9081d353f7a2625b18683db3ac0c7ebc2e5fa315
Simon Cruanes <simon.cruanes.2007@m4x.org>
2020-09-23 17:21:16 -0400

    make tests silent

--- tests/check_regs.ml
+++ tests/check_regs.ml
@@ -2,7 +2,7 @@
 let ok = ref true
 let test file =
   Printf.eprintf "test file '%s'\n%!" file;
-  let code = Sys.command ("../src/bin/smtlib_cat.exe " ^ file) in
+  let code = Sys.command ("../src/bin/smtlib_cat.exe -q " ^ file) in
   if code <> 0 then (
     Printf.eprintf "> file '%s': code=%d\n%!" file code;
     ok := false
